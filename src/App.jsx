ba7781f9bd8ea9715f85af34ce35ebf9abbb4572
Marco Ng <marcong95@163.com>
2018-11-27 17:59:19 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import HtmlEntities from './components/HtmlEntities'
+
+export default class ReactPlayground extends Component {
+  render() {
+    return (
+      <div className="react-playground">
+        <HtmlEntities />
+      </div>
+    );
+  }
+}
---
+++ src/components/HtmlEntities.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+import '../styles/HtmlEntities.styl'
+
+export default class HtmlEntities extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      html: '',
+      text: ''
+    }
+
+    this.midTextarea = document.createElement('textarea')
+  }
+
+  render = () => (
+    <div
+      className="html-entities">
+      <div
+        className="text-input-wrap">
+        <textarea
+          id="html-input"
+          className="text-input-textarea"
+          rows="10"
+          value={this.state.html}
+          onChange={this.htmlInput}></textarea>
+      </div>
+      <div
+        className="text-input-labels">
+        <label htmlFor="html-input">HTML &uarr;</label>
+        <label htmlFor="text-input">Text &darr;</label>
+      </div>
+      <div
+        className="text-input-wrap">
+        <textarea
+          id="text-input"
+          className="text-input-textarea"
+          rows="10"
+          value={this.state.text}
+          onChange={this.textInput}></textarea>
+      </div>
+    </div>
+  )
+
+  htmlInput = e => {
+    const html = e.target.value
+    this.midTextarea.innerHTML = html
+    const text = this.midTextarea.innerText
+    this.setState({
+      html,
+      text
+    })
+  }
+
+  textInput = e =>  {
+    const text = e.target.value
+    this.midTextarea.innerText = text
+    const html = this.midTextarea.innerHTML
+    this.setState({
+      html,
+      text
+    })
+  }
+}
