a8e7017e0e365de432c50f3a31dfe2adea39bfdf
Marco Ng <marcong95@163.com>
2019-02-21 17:43:51 +0800

    PokeTypes: add highlight row & col on hover

--- src/gadgets/PokeTypes.jsx
+++ src/gadgets/PokeTypes.jsx
@@ -23,8 +23,8 @@ const effect = (atkType, defType) => {
 const formatTypeName = type => type.replace(/(ing|ic)$/, '');
 
 const HighlightContext = React.createContext({
-  atk: 'fighting',
-  def: 'dragon',
+  atk: undefined,
+  def: undefined,
   setHighlight: () => {}
 })
 
@@ -39,31 +39,65 @@ const DefTypeName = ({type: def}) => (<th className={styles.defType}
 class EffectDisplay extends Component {
   render = () => {
     const {atk, def} = this.props
-    return (<td className={styles.effect}
-      onMouseEnter={this.handleHover}>{effect(atk, def)}</td>)
+    return (
+      <HighlightContext.Consumer>
+        {({atk: hlAtk, def: hlDef, setHighlight}) => (
+          <td className={styles.effect}
+            onMouseEnter={event => this.handleHover(setHighlight, event)}
+            style={{ backgroundColor: atk === hlAtk || def === hlDef ? '#ccc' : 'transparent'}}
+            >{effect(atk, def)}</td>
+        )}
+      </HighlightContext.Consumer>
+    )
   }
 
-  handleHover = event => {}
+  handleHover = (setHighlight, event) => {
+    setHighlight(this.props.atk, this.props.def)
+  }
 }
 
-const AtkType = ({type: atk}) => (<tr key={atk}>
+const AtkType = ({type: atk}) => (<tr>
   <AtkTypeName type={atk}></AtkTypeName>
   {types.map(def => (<EffectDisplay key={def}
     {...{atk, def}}></EffectDisplay>))}
 </tr>)
 
 export default class PokeTypes extends Component {
+  static contextType = HighlightContext
+
+  state = {
+    highlight: {
+      atk: undefined,
+      def: undefined,
+      setHighlight: (atk, def) => {
+        this.state.highlight.atk = atk;
+        this.state.highlight.def = def;
+        this.setState({
+          highlight: this.state.highlight
+        })
+      }
+    }
+  }
+
   render = () => (
-    <div>
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            {types.map(t => (<DefTypeName type={t}></DefTypeName>))}
-          </tr>
-          {types.map(t => (<AtkType type={t}></AtkType>))}
-        </tbody>
-      </table>
+    <div onMouseLeave={this.downplay}>
+      <HighlightContext.Provider value={this.state.highlight}>
+        <table className={styles.pokeTypeTable}>
+          <tbody>
+            <tr>
+              <th></th>
+              {types.map(t => (<DefTypeName key={t}
+                type={t}></DefTypeName>))}
+            </tr>
+            {types.map(t => (<AtkType key={t}
+              type={t}></AtkType>))}
+          </tbody>
+        </table>
+      </HighlightContext.Provider>
     </div>
   )
+
+  downplay = () => {
+    this.state.highlight.setHighlight(undefined, undefined)
+  }
 }
